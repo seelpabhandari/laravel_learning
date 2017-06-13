e28eb5c43a70f8c1bbe843c4f4488f9a30aafda4
seelpabhandari <seelpabhandari@gmail.com>
2017-06-13 09:26:51 +0545

    add all

--- app/Tag.php
+++ app/Tag.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+       public function posts()
+     {
+        // any task may have many tags
+        //any tag may be applied to
+       return $this->belongsToMany(Post::class);
+     }
 }
